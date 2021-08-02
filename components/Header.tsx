30e2f428900bab5f6e0b5eea05d45c5dde64e63c
desteban <desteban28c@hotmail.com>
2021-08-01 22:36:14 -0500

    menu movil

--- components/Header.tsx
+++ components/Header.tsx
@@ -10,17 +10,41 @@ export default class Header extends Component {
 		return (
 			<header>
 				<nav>
-					<div className="log">{`('desteban')`}</div>
+					<div className="btn-menu" id="btn-menu" onClick={() => this.toogleMenu()}>
+						<span id="abrirMenu" className="material-icons click">
+							menu
+						</span>
+						<span id="cerrarMenu" className="material-icons hide click">
+							close
+						</span>
+					</div>
+					<div className="logo">{`('desteban')`}</div>
 
-					<div className="sidenav">
-						<div className="contenido">
-							<Link href="/">
-								<a>Inicio</a>
-							</Link>
-						</div>
+					<div className="sidenav" id="sidenav" onClick={() => this.toogleMenu()}>
+						<Link href="/">
+							<a className="item">Inicio</a>
+						</Link>
+
+						<Link href="/">
+							<a className="item">Blog</a>
+						</Link>
+
+						<Link href="/">
+							<a className="item">Proyectos</a>
+						</Link>
 					</div>
 				</nav>
 			</header>
 		);
+	}
+
+	toogleMenu() {
+		let sidenav = document.getElementById('sidenav');
+		let abrirMenu = document.getElementById('abrirMenu');
+		let cerrarMenu = document.getElementById('cerrarMenu');
+
+		sidenav?.classList.toggle('open');
+		abrirMenu?.classList.toggle('hide');
+		cerrarMenu?.classList.toggle('hide');
 	}
 }
