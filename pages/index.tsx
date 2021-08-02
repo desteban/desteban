db82af603df5a5b51e7c3c5724e7e4a4d1ad2a2f
desteban <desteban28c@hotmail.com>
2021-08-01 19:19:57 -0500

    estilos basicos

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,12 @@ export default function Home() {
 		<div>
 			<main>
 				<h1>Hola que tal</h1>
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque id quam unde
+					molestias perspiciatis sapiente quibusdam veritatis laborum reiciendis
+					consequuntur exercitationem rerum, quo repudiandae blanditiis animi dolor dolore
+					perferendis voluptate. <a href="">Link</a>
+				</p>
 			</main>
 		</div>
 	);
