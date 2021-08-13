cfffc5cd516f6ab418f38677b90f30f009938ddc
desteban <desteban28c@hotmail.com>
2021-08-13 13:32:34 -0500

    cambiar el alt de las imagenes de las skills

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -9,42 +9,47 @@ export default function Skills() {
 			</div>
 
 			<div className="skill">
-				<Image alt="HTML" src={'/svg/skills/css.svg'} height={40} width={40} />
+				<Image alt="CSS 3" src={'/svg/skills/css.svg'} height={40} width={40} />
 				<span>CSS 3</span>
 			</div>
 
 			<div className="skill">
-				<Image alt="HTML" src={'/svg/skills/javascript.svg'} height={40} width={40} />
+				<Image alt="JavaScript" src={'/svg/skills/javascript.svg'} height={40} width={40} />
 				<span>JavaScript</span>
 			</div>
 
 			<div className="skill">
-				<Image alt="HTML" src={'/svg/skills/node.svg'} height={40} width={60} />
+				<Image alt="Node.js" src={'/svg/skills/node.svg'} height={40} width={60} />
 				<span>Node.js</span>
 			</div>
 
 			<div className="skill">
-				<Image alt="HTML" src={'/svg/skills/typescript.svg'} height={40} width={40} />
+				<Image alt="TypeScript" src={'/svg/skills/typescript.svg'} height={40} width={40} />
 				<span>TypeScript</span>
 			</div>
 
 			<div className="skill">
-				<Image alt="HTML" src={'/svg/skills/git.svg'} height={40} width={40} />
+				<Image alt="Git" src={'/svg/skills/git.svg'} height={40} width={40} />
 				<span>Git</span>
 			</div>
 
 			<div className="skill">
-				<Image alt="HTML" src={'/svg/skills/sass.svg'} height={40} width={60} />
+				<Image alt="SASS" src={'/svg/skills/sass.svg'} height={40} width={60} />
 				<span>SASS</span>
 			</div>
 
 			<div className="skill">
-				<Image alt="HTML" src={'/svg/skills/materializecss.svg'} height={40} width={40} />
+				<Image
+					alt="Materializecss"
+					src={'/svg/skills/materializecss.svg'}
+					height={40}
+					width={40}
+				/>
 				<span>Materializecss</span>
 			</div>
 
 			<div className="skill">
-				<Image alt="HTML" src={'/svg/skills/mysql.svg'} height={40} width={40} />
+				<Image alt="MySQL" src={'/svg/skills/mysql.svg'} height={40} width={40} />
 				<span>MySQL</span>
 			</div>
 		</div>
