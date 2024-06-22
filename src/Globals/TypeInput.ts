ed5ed3394dfd80a8b1f3da34ec0e287a7be7c5ed
desteban <esteban28ec@outlook.com>
2024-06-21 20:57:08 -0500

    inputs para pruebas de puppetier

---
+++ src/Globals/TypeInput.ts
@@ -0,0 +1,9 @@
+export type TypeInput = "text" | "email" | "password" | "button" | "number";
+
+export enum TypesInput {
+  Text = "text",
+  Email = "email",
+  Password = "Password",
+  Button = "button",
+  Number = "number",
+}
