b090afae2431e8cab139006c5e9daf4b5d03dcd2
desteban <esteban28ec@outlook.com>
2025-07-10 23:23:48 -0500

    feat: enhance SEO with keywords and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,43 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
+
+const changefreq = {
+  always: "always",
+  hourly: "hourly",
+  daily: "daily",
+  weekly: "weekly",
+  monthly: "monthly",
+  yearly: "yearly",
+  never: "never",
+};
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  site: "https://desteban.tech",
+  integrations: [
+    tailwind(),
+    react(),
+    sitemap({
+      serialize(item) {
+        // if (/regex/.test(item.url)) {
+        //   item.lastmod = new Date();
+        //   item.priority = 1;
+        // }
+
+        if (/proyectos\/luks/.test(item.url)) {
+          return undefined;
+        }
+
+        item.lastmod = new Date();
+        item.changefreq = changefreq.monthly;
+        item.priority = 1;
+
+        return item;
+      },
+    }),
+  ],
 });
+
+// https://docs.astro.build/es/guides/integrations-guide/sitemap/
